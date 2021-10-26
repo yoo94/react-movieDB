a363d18e8f5ce5315ee0da8f254aac4dfbda6b7f
yoo94 <ahy0119@naver.com>
2021-10-26 23:16:28 +0900

    비밀정보보호

--- index.js
+++ index.js
@@ -2,15 +2,41 @@ const express = require('express')
 const app = express()
 const port = 5000
 
+const bodyParser = require('body-parser')
+
+const config = require('./config/key')
+
+const {User} = require("./models/user")
+
+//application/x-www.form-urlencoded
+app.use(bodyParser.urlencoded({extended: true}));
+
+//application.json
+app.use(bodyParser.json());
+
 
 const mongoose = require('mongoose')
-mongoose.connect('mongodb+srv://yoo94:dbwotjr0329@reactbase.49thc.mongodb.net/myFirstDatabase?retryWrites=true&w=majority')
+mongoose.connect(config.mongoURI)
     .then(() => console.log('success connect mongoDB...'))
     .catch(err => console.log(err))
 
 app.get('/', (req, res) => {
-    res.send('Hello World! yoo94')
+    res.send('Hello World! hi')
 })
+
+//회원가입을 위한 라우터
+app.post('/register', (req, res) => {
+
+    //회원가입 할때에 필요한 정보들을 클라이언트에서 가져오면 그것들을 데이터베이스에 넣어준다.
+    const user = new User(req.body);
+    user.save((err, userInfo) => {
+        if (err) return res.json({success: false, err})
+        return res.status(200).json({
+            success: true
+        })
+    })
+})
+
 
 app.listen(port, () => {
     console.log(`Example app listening at http://localhost:${port}`)
