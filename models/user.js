f66d8c9cfa7fd651ce866fba0c3581381f86ddf5
yoo94 <ahy0119@naver.com>
2021-10-26 23:38:17 +0900

    비밀번호 암호화

--- models/user.js
+++ models/user.js
@@ -1,4 +1,6 @@
 const mongoose = require('mongoose');
+const bcrypt = require('bcrypt');
+const saltRounds = 10;
 
 const userSchema = mongoose.Schema({
     name: {
@@ -28,6 +30,26 @@ const userSchema = mongoose.Schema({
     },
     tokenExp: {
         type: Number
+    }
+})
+
+userSchema.pre('save', function (next) {
+    var user = this;
+
+    if (user.isModified('password')) {
+        //암호화
+        bcrypt.genSalt(saltRounds, function (err, salt) {
+            if (err) return next(err)
+            bcrypt.hash(user.password, salt, function (err, hash) {
+                // Store hash in your password DB.
+                if (err) return next(err)
+                user.password = hash
+                next()
+            })
+        });
+
+    } else {
+        next();
     }
 })
 
