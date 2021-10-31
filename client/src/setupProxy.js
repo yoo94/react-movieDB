b65f22a6d2579d34d6b456b0893095bd3ecf09e0
yoo94 <ahy0119@naver.com>
2021-10-31 13:13:40 +0900

    react axios/ proxy

---
+++ client/src/setupProxy.js
@@ -0,0 +1,17 @@
+const proxy = require('http-proxy-middleware');
+
+module.exports = function (app) {
+
+    app.use(
+        '/api',
+
+        proxy({
+
+            target: 'http://localhost:5000',
+
+            changeOrigin: true,
+
+        })
+    );
+
+};
